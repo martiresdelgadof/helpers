c5cb413373725225c360edff9c7d796366504c8a
unknown <martiresdelgadof@gmail.com>
2019-04-03 22:04:57 -0400

    first commit

---
+++ helpers/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using helpers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace helpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Formulario()
+        {
+            Persona p = new Persona();
+            return View(p);
+        }
+        [HttpPost]
+        public IActionResult Formulario(Persona persona) {
+            
+            return View(persona);
+        }
+    }
+}
---
+++ helpers/Models/Persona.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helpers.Models
+{
+    public class Persona
+    {
+        [Required]
+        [MaxLength(13)]
+        [DataType(DataType.Text)]
+        public string cedula { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DataType(DataType.Text)]
+        public string nombre { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DataType(DataType.Text)]
+        public string apellido { get; set; }
+
+        [Required]
+        public int edad { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        [DataType(DataType.PhoneNumber)]
+        public string telefono { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [DataType(DataType.EmailAddress)]
+        public string correo { get; set; }
+
+        [Required]
+        public string genero { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string estado { get; set; }
+
+        [Required]
+        public string hobbys { get; set; }
+    }
+}
